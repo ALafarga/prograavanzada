40ea2586d6dfb0f6eb4d42a0f37b9710099b4be6
Art <Lafarga@Arts-MacBook-Pro.local>
2018-05-06 17:47:31 -0500

    mac upload

--- proyectos/proyecto3/marriage.c
+++ proyectos/proyecto3/marriage.c
@@ -1,10 +1,48 @@
 #include <stdio.h>
-
 typedef struct s{
-    unsigned char order:
-}prefItem;
+    unsigned char used;
+    unsigned short *preferencia;
+    unsigned short count;
+}prefItem_t;//prefItem_t||PrefItem
+
+void checkOnWomenHusband(prefItem_t *mujeres, prefItem_t *hombres, short n){
+    short i = 0;
+    for(i = 0; i < n; i++){
+        if(mujeres[i].preferencia[0] == i){
+            mujeres[i].used = 1;
+            hombres[i].used = 1;
+        }
+    }
+}
+
+void iterateWomen(prefItem_t *mujeres, prefItem_t *hombres, short n){
+    short i = 0;
+    for(i = 0; i < n; i++){
+        if(mujeres[i].used == 0){
+            if(hombres[
+                mujeres[i].preferencia[mujeres[i].count]
+                ].preferencia[[mujeres[i].preferencia[mujeres[i].count]]] == mujeres[i].preferencia[mujeres[i].count]){
+
+            }
+
+        }
+    }
+}
 
 int main (){
+    prefItem_t *mujeres;
+    prefItem_t *hombres;
+    short n,  //cantidad de parejas
+        t;  //cantidad de test cases
+    short i = 0;
+
+    mujeres = (prefItem_t *)calloc(n, sizeof(prefItem_t));//calloc inicializa las variables
+    hombres = (prefItem_t *)calloc(n, sizeof(prefItem_t));
+    for(i = 0; i < n; i++){
+        mujeres[i].preferencia = (short *)calloc(n, sizeof(short));
+        hombres[i].preferencia = (short *)calloc(n, sizeof(short));
+    }
+
 
     return 0;
 }
